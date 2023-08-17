a2e58a31d33ac2700d032fd1637d10a4da97150c
MonkasteR <monkaster@gmail.com>
2023-08-17 17:43:37 +0300

    Дописал типы данных

--- start.py
+++ start.py
@@ -1,17 +1,17 @@
-import numpy as np
-import timeit
 import multiprocessing
+import timeit
 
+import numpy as np
 from numpy import ndarray
 
-start_time = timeit.default_timer()
+start_time: float = timeit.default_timer()
 
-def should_remove_block(lines, values_to_check, values_to_address):
-    check_found = any(value in str(lines) for value in values_to_check)
-    poi_or_polygon_found = any('[POI]' in line or '[POLYGON]' in line for line in lines)
-    
+
+def should_remove_block(lines, values_to_check, values_to_address) -> bool:
+    check_found: bool = any(value in str(lines) for value in values_to_check)
+    poi_or_polygon_found = any("[POI]" in line or "[POLYGON]" in line for line in lines)
+
     return check_found or (poi_or_polygon_found and not all(addr in str(lines) for addr in values_to_address))
-
 
 
 def replace_values(lines, values_to_replace):
@@ -31,15 +31,15 @@ def process_block(block, values_to_check, values_to_replace, values_to_address):
 
 
 def process_file(in_file, values_to_check, values_to_replace, values_to_address):
-    out_file = in_file.replace('.mp', '_out.mp')
-    with open(in_file, 'r') as f_in, open(out_file, 'w') as f_out:
+    out_file = in_file.replace(".mp", "_out.mp")
+    with open(file=in_file, mode="r") as f_in, open(file=out_file, mode="w") as f_out:
         block = False
         content = []
         for line in f_in:
-            if line.startswith('; WayID') or line.startswith('; NodeID'):
+            if line.startswith("; WayID") or line.startswith("; NodeID"):
                 block = True
-                temp_block = line
-            elif block and line.strip() == '[END]':
+                temp_block: str = line
+            elif block and line.strip() == "[END]":
                 block = False
                 if should_remove_block(content, values_to_check, values_to_address):
                     content = []
@@ -56,30 +56,31 @@ def process_file(in_file, values_to_check, values_to_replace, values_to_address)
     f_out.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from multiprocessing import freeze_support
 
     freeze_support()
 
-    with open('in_files.txt', 'r') as f:
+    with open(file="in_files.txt", mode="r") as f:
         in_files = [line.strip() for line in f.readlines()]
-        print('Читаем список обрабатываемых файлов \n')
+        print("Читаем список обрабатываемых файлов \n")
 
-    with open('values_to_check.txt', 'r') as f:
+    with open(file="values_to_check.txt", mode="r") as f:
         values_to_check = [line.strip() for line in f.readlines()]
-        print('Читаем список слов удаляющих блок \n')
+        print("Читаем список слов удаляющих блок \n")
 
-    with open('values_to_replace.txt', 'r') as f:
+    with open(file="values_to_replace.txt", mode="r") as f:
         values_to_replace = [line.strip() for line in f.readlines()]
-        print('Читаем список слов для замены \n')
+        print("Читаем список слов для замены \n")
 
-    with open('values_to_address.txt', 'r') as f:
-        values_to_address = [line.strip() for line in f.readlines()]
-        print('Читаем список слов удаляющих полигон \n')
+    with open(file="values_to_address.txt", mode="r") as f:
+        values_to_address: list[str] = [line.strip() for line in f.readlines()]
+        print("Читаем список слов удаляющих полигон \n")
 
     with multiprocessing.Pool() as pool:
-        pool.starmap(process_file,
-                     [(in_file, values_to_check, values_to_replace, values_to_address) for in_file in in_files])
+        pool.starmap(
+            process_file, [(in_file, values_to_check, values_to_replace, values_to_address) for in_file in in_files]
+        )
 
-    end_time = timeit.default_timer()
+    end_time: float = timeit.default_timer()
     print("Время выполнения программы: {:.5f} секунд".format(end_time - start_time))
