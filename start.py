d2f3d7f2d4272a668e94c8b5aa70cfe193ff0983
MonkasteR <monkaster@gmail.com>
2023-09-11 15:44:17 +0300

    Подправил код, добавил описания

--- start.py
+++ start.py
@@ -1,44 +1,95 @@
 import multiprocessing
 import timeit
-
+from typing import Any, List, Dict
 import numpy as np
 from numpy import ndarray
 
 start_time: float = timeit.default_timer()
 
 
-def should_remove_block(lines, values_to_check, values_to_address) -> bool:
-    check_found: bool = any(value in str(lines) for value in values_to_check)
+def should_remove_block(lines: List[str], values_to_check: List[str], values_to_address: List[str]) -> bool:
+    """
+    Проверяет, должен ли блок строк быть удален на основе определенных условий.
+
+    Аргументы:
+        lines (List[str]): Список строк для проверки.
+        values_to_check (List[str]): Список значений для проверки наличия в строках.
+        values_to_address (List[str]): Список значений для обработки, если они присутствуют в строках.
+
+    Возвращает:
+        bool: True, если блок должен быть удален, False в противном случае.
+    """
+    check_found = any(value in line for line in lines for value in values_to_check)
     poi_or_polygon_found = any("[POI]" in line or "[POLYGON]" in line for line in lines)
+    addr_found = any(addr in line for line in lines for addr in values_to_address)
 
-    return check_found or (poi_or_polygon_found and not all(addr in str(lines) for addr in values_to_address))
+    return check_found or (poi_or_polygon_found and not addr_found)
 
 
-def replace_values(lines, values_to_replace):
-    arr: ndarray = np.array(lines)
+def replace_values(lines: List[str], values_to_replace: List[str]) -> List[str]:
+    """
+    Заменяет определенные значения в списке строк.
+
+    Аргументы:
+        lines (List[str]): Список строк для обработки.
+        values_to_replace (List[str]): Список значений для замены.
+
+    Возвращает:
+        List[str]: Измененный список строк с замененными значениями.
+    """
+    arr = np.array(lines)
     mask = np.zeros(len(arr), dtype=bool)
     for value in values_to_replace:
         mask = np.logical_or(mask, [value in line for line in arr])
     return list(arr[~mask])
 
 
-def process_block(block, values_to_check, values_to_replace, values_to_address):
+def process_block(block: List[str], values_to_check: List[str], values_to_replace: List[str],
+                  values_to_address: List[str]) -> List[str]:
+    """
+    Обрабатывает блок кода.
+
+    Аргументы:
+        block (List[str]): Блок кода для обработки.
+        values_to_check (List[str]): Значения, которые нужно проверить в блоке.
+        values_to_replace (List[str]): Значения, которые нужно заменить в блоке.
+        values_to_address (List[str]): Значения, которые нужно обработать в блоке.
+
+    Возвращает:
+        List[str]: Обработанный блок кода.
+    """
     if should_remove_block(block, values_to_check, values_to_address):
         return []
-    else:
-        processed_block = replace_values(block, values_to_replace)
-        return processed_block
+    processed_block = replace_values(block, values_to_replace)
+    return processed_block
 
 
-def process_file(in_file, values_to_check, values_to_replace, values_to_address):
+def process_file(
+        in_file: str,
+        values_to_check: List[str],
+        values_to_replace: Dict[str, str],
+        values_to_address: List[str]
+) -> None:
+    """
+    Обрабатывает входной файл и записывает выходной файл.
+
+    Аргументы:
+        in_file (str): Путь к входному файлу.
+        values_to_check (List[str]): Список значений для проверки в каждом блоке.
+        values_to_replace (Dict[str, str]): Словарь значений для замены в каждом блоке.
+        values_to_address (List[str]): Список значений для обработки в каждом блоке.
+
+    Возвращает:
+        None
+    """
     out_file = in_file.replace(".mp", "_out.mp")
-    with open(file=in_file, mode="r") as f_in, open(file=out_file, mode="w") as f_out:
+    with open(in_file, "r") as f_in, open(out_file, "w") as f_out:
         block = False
         content = []
         for line in f_in:
             if line.startswith("; WayID") or line.startswith("; NodeID"):
                 block = True
-                temp_block: str = line
+                temp_block = line
             elif block and line.strip() == "[END]":
                 block = False
                 if should_remove_block(content, values_to_check, values_to_address):
@@ -52,8 +103,6 @@ def process_file(in_file, values_to_check, values_to_replace, values_to_address)
                 content.append(line)
             else:
                 f_out.write(line)
-    f_in.close()
-    f_out.close()
 
 
 if __name__ == "__main__":
